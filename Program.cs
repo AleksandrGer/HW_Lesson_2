3222f5677e2ecb042f339bf0f19a80a7e001738b
Aleksandr G <gerbl444@gmail.com>
2023-11-06 18:35:46 +0300

    ДЗ Урок 4. Функции

--- Program.cs
+++ Program.cs
@@ -1,45 +1,51 @@
-﻿
-// Задача 10.
-
-// int number = new Random() .Next(100, 1000);
-// int secondDigit = number % 100 / 10;
-// Console.WriteLine($"Число {number}, вторая цифра {secondDigit}");
-
-
-// Задача 13.
-
-// Console.Write("Введите число: ");
-// string stringNumber = Console.ReadLine();
-// int number;
-// bool IsNumber = int.TryParse(stringNumber, out number);
-// if (IsNumber && stringNumber.Length >= 3)
-// {
-//   Console.WriteLine("В вашей строчке только цифры и оно имеет третий знак! *Correct*");
-//   Console.WriteLine($"Число {stringNumber}, третья цифра {stringNumber[2]}");
-// }
-// else
-// {
-//   Console.WriteLine("Введены посторонние символы или нет третьей цифры! *Fix*");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите степень:");
+// int pow = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Число {number} в степени {pow} = {GetPow(number, pow)}");
+// int GetPow (int n, int p)
+// { 
+//   int res = 0;
+//   for (int i = 1; i < p; i++)
+//   { 
+//     res = n * n;
+//   }
+//   return res;
 // }
 
-
-// Задача 15.
-
-// Console.Write("Введите номер дня недели от 1 до 7: ");
-// string stringNumber = Console.ReadLine();
-// int number;
-// bool IsNumber = int.TryParse(stringNumber, out number);
-// if (IsNumber && number == 6 || number == 7)
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// Console.Write("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Сумма цифр в числе {number} = {GetSum(number)}");
+// int GetSum (int num)
 // {
-//   Console.WriteLine("Это выходной день!!!");
-
+//     int sum = 0;
+//     while(num > 0)
+//     {
+//         sum += num % 10;
+//         num /= 10;
+//     }
+//     return sum;
 // }
-// if (IsNumber && number < 6 && number > 1)
-// {
-//   Console.WriteLine("Это будний день...");
 
-// }
-// if(IsNumber == false || number > 7 || number < 1)
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// int[] result = GetBinaryArray(8);
+// Console.WriteLine($"[{String.Join("; ", result)}]");
+// int [] GetBinaryArray(int size)
 // {
-//   Console.WriteLine("Введены посторонние символы или число вне диапазона!");
+//     int [] array = new int[size];
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(99, 1000);
+//     }
+//     return array;
 // }
